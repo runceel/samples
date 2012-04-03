c99a86e309e07ae0638650b9d2c5396f6b4b4bc8
Kazuki Oota <k.ota.0130@gmail.com>
2012-04-03 21:10:10 +0900

    add EntLib sample

---
+++ EntLib5Samples/EntLib5Samples.HelloWorld01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EntLib5Samples.HelloWorld01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
