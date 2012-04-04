46602b76a56a8fbbba3521f4c0bfdd22dd4543d7
Kazuki Oota <k.ota.0130@gmail.com>
2012-04-04 23:56:10 +0900

    add sample

---
+++ EntLib5Samples/01-HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+using Microsoft.Practices.EnterpriseLibrary.Logging.TraceListeners;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
+
+namespace HelloWorld
+{
+    class MyTraceListener : CustomTraceListener
+    {
+        public override void Write(string message)
+        {
+            Console.Write(message);
+        }
+
+        public override void WriteLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationSourceBuilder();
+            builder.ConfigureLogging()
+                .WithOptions.DoNotRevertImpersonation()
+                .LogToCategoryNamed("HelloWorldCategory")
+                    .SendTo.Custom<MyTraceListener>("sample")
+                    .FormatWith(new FormatterBuilder().TextFormatterNamed("Text Formatter").UsingTemplate(
+                        "Message: {message}{newline}Category: {category}{newline}Priority: {priority}{newline}"));
+
+            var config = new DictionaryConfigurationSource();
+            builder.UpdateConfigurationWithReplace(config);
+
+            EnterpriseLibraryContainer.Current = EnterpriseLibraryContainer.CreateDefaultContainer(config);
+
+            var logger = EnterpriseLibraryContainer.Current.GetInstance<LogWriter>();
+            logger.Write("Hello world", "HelloWorldCategory", 2, 9999);
+        }
+    }
+}
