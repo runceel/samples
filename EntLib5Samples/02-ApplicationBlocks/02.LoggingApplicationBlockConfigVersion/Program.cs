75ffe594c4a411c317b10548584e1088098fff42
Kazuki Oota <k.ota.0130@gmail.com>
2012-04-11 23:50:57 +0900

    add db

--- EntLib5Samples/02-ApplicationBlocks/02.LoggingApplicationBlockConfigVersion/Program.cs
+++ EntLib5Samples/02-ApplicationBlocks/02.LoggingApplicationBlockConfigVersion/Program.cs
@@ -1,14 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace _02.LoggingApplicationBlockConfigVersion
+﻿namespace _02.LoggingApplicationBlockConfigVersion
 {
+    using System.Diagnostics;
+    using System.Linq;
+    using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+    using Microsoft.Practices.EnterpriseLibrary.Logging;
+
     class Program
     {
         static void Main(string[] args)
         {
+            // 構成ファイル(app.config)から作成されたコンテナからLogWriterを取得
+            var l = EnterpriseLibraryContainer.Current.GetInstance<LogWriter>();
+            // Verbose～Criticalまでのログを出力
+            l.Write(new LogEntry { Message = "VerboseMessage", Severity = TraceEventType.Verbose });
+            l.Write(new LogEntry { Message = "InformationMessage", Severity = TraceEventType.Information });
+            l.Write(new LogEntry { Message = "WarningMessage", Severity = TraceEventType.Warning });
+            l.Write(new LogEntry { Message = "ErrorMessage", Severity = TraceEventType.Error });
+            l.Write(new LogEntry { Message = "CriticalMessage", Severity = TraceEventType.Critical });
+            // 出力したログの表示
+            Process.Start("default.log");
+
+            // ローリングさせるために大量のログを出力
+            foreach (var i in Enumerable.Range(1, 100000))
+            {
+                l.Write("ログメッセージ", "Rolling");
+                l.Write(new LogEntry { Message = "sample message", Categories = { "Rolling" } });
+            }
+
+            // イベントログの出力
+            l.Write("EventLogMessage", "EventLog", 0, 0, TraceEventType.Information);
         }
     }
 }
---
+++ EntLib5Samples/02-ApplicationBlocks/03.DatabaseSample/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace DatabaseSample
+{
+    using System;
+    using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+    using Microsoft.Practices.EnterpriseLibrary.Data;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 単純にSQL文を実行
+            {
+                // Enterprise LibraryのコンテナからDatabaseクラスのインスタンスを取得
+                var database = EnterpriseLibraryContainer.Current.GetInstance<Database>();
+                // SQL文を発行してデータを格納
+                var people = database.ExecuteSqlStringAccessor<Person>(
+                    "SELECT Id, Name, Age FROM PERSON ORDER BY Age DESC");
+                // 結果を表示
+                foreach (var p in people)
+                {
+                    Console.WriteLine("Id: {0}, Name: {1}, Age: {2}", p.Id, p.Name, p.Age);
+                }
+            }
+            // パラメータつきのSQL文
+            {
+                // Enterprise LibraryのコンテナからDatabaseクラスのインスタンスを取得
+                var database = EnterpriseLibraryContainer.Current.GetInstance<Database>();
+                // SQL文を発行してデータを格納
+                var accessor = database.CreateSqlStringAccessor<Person>(
+                    "SELECT Id, Name, Age FROM PERSON WHERE NAME LIKE @p1 ORDER BY Age DESC");
+                var people = accessor.Execute("%mu%");
+                // 結果を表示
+                foreach (var p in people)
+                {
+                    Console.WriteLine("Id: {0}, Name: {1}, Age: {2}", p.Id, p.Name, p.Age);
+                }
+            }
+
+        }
+    }
+
+    class Person
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
