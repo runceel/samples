36a573bb6ea8400afbab47a471ab1e0a0a48391f
Kazuki Oota <k.ota.0130@gmail.com>
2012-04-16 23:07:29 +0900

    data access application block追記

--- EntLib5Samples/02-ApplicationBlocks/03.DatabaseSample/Program.cs
+++ EntLib5Samples/02-ApplicationBlocks/03.DatabaseSample/Program.cs
@@ -10,12 +10,15 @@
     using System.Data.SqlClient;
     using System.Data.SqlServerCe;
     using System.Configuration;
+    using Microsoft.Practices.EnterpriseLibrary.Data.Sql;
+    using EntLib5Sample.Commons;
 
     class Program
     {
         static void Main(string[] args)
         {
             // 単純にSQL文を実行
+            Console.WriteLine("---------------------------------------");
             {
                 // Enterprise LibraryのコンテナからDatabaseクラスのインスタンスを取得
                 var database = EnterpriseLibraryContainer.Current.GetInstance<Database>();
@@ -29,13 +32,14 @@
                 }
             }
             // パラメータつきのSQL文
+            Console.WriteLine("---------------------------------------");
             {
                 // Enterprise LibraryのコンテナからDatabaseクラスのインスタンスを取得
                 var database = EnterpriseLibraryContainer.Current.GetInstance<Database>();
                 // SQL文を発行してデータを格納
                 var accessor = database.CreateSqlStringAccessor<Person>(
                     // パラメータつきのSQL文
-                    "SELECT Id, Name, Age FROM PERSON WHERE NAME LIKE @p0 ORDER BY ID DESC",
+                    "SELECT Id, Name, Age FROM PERSON WHERE NAME LIKE @p1 ORDER BY ID DESC",
                     // パラメータのマッピングルール
                     SequenceParameterMapper.Default);
                 // パラメータを指定して実行
@@ -47,45 +51,137 @@
                 }
             }
             // データの登録
+            Console.WriteLine("---------------------------------------");
             {
                 // Enterprise LibraryのコンテナからDatabaseクラスのインスタンスを取得
                 var database = EnterpriseLibraryContainer.Current.GetInstance<Database>();
-                var p = new Person { Name = "hanami", Age = 100 };
-                var count = database.ExecuteUpdate(
-                    "INSERT INTO PERSON(NAME, AGE) VALUES(@p0, @p1)",
-                    SequenceParameterMapper.Default,
-                    p.Name, p.Age);
+                // コネクションを作成
+                using (var conn = database.CreateConnection())
+                {
+                    conn.Open();
+                    // トランザクションを開始
+                    using (var tran = conn.BeginTransaction())
+                    {
+                        // 登録対象のデータ
+                        var p = new Person { Name = "hanami", Age = 100 };
+                        // コマンドをSQLから作成
+                        var command = database.GetSqlStringCommand("INSERT INTO PERSON(NAME, AGE) VALUES(@p1, @p2)");
+                        // パラメータを追加
+                        database.AddInParameter(command, "p1", DbType.String, p.Name);
+                        database.AddInParameter(command, "p2", DbType.Int32, p.Age);
+                        // トランザクションを指定してコマンドを実行
+                        var count = database.ExecuteNonQuery(command, tran);
 
-                Console.WriteLine(count);
+                        // DB側でふられたIDを取得
+                        var newId = database.ExecuteScalar(tran, CommandType.Text, "SELECT @@IDENTITY");
+                        // 登録件数と、登録時にふられたIDを表示
+                        Console.WriteLine("inserted: {0}, newId: {1}", count, newId);
+                        // コミット
+                        tran.Commit();
+                    }
+                    conn.Close();
+                }
             }
             // Transaction
+            Console.WriteLine("---------------------------------------");
             {
                 // Enterprise LibraryのコンテナからDatabaseクラスのインスタンスを取得
-                using (var tc = new TransactionScope(TransactionScopeOption.Required))
+                using (var tc = new TransactionScope())
                 {
+                    var database1 = EnterpriseLibraryContainer.Current.GetInstance<Database>();
+                    Console.WriteLine("最初のdatabaseオブジェクト取得 HashCode: {0}", database1.GetHashCode());
+                    for (int i = 0; i < 2; i++)
                     {
-                        var database = EnterpriseLibraryContainer.Current.GetInstance<Database>();
-                        for (int i = 0; i < 100; i++)
-                        {
-                            var p = new Person { Name = "inoue", Age = 100 };
-                            var count = database.ExecuteUpdate(
-                                "INSERT INTO PERSON(NAME, AGE) VALUES(@p0, @p1)",
-                                SequenceParameterMapper.Default,
-                                p.Name, p.Age);
-                        }
+                        // 登録対象のデータ
+                        var p = new Person { Name = "hanami", Age = 100 };
+                        // コマンドをSQLから作成
+                        var command = database1.GetSqlStringCommand("INSERT INTO PERSON(NAME, AGE) VALUES(@p1, @p2)");
+                        // パラメータを追加
+                        database1.AddInParameter(command, "p1", DbType.String, p.Name);
+                        database1.AddInParameter(command, "p2", DbType.Int32, p.Age);
+                        // トランザクションを指定してコマンドを実行
+                        var count = database1.ExecuteNonQuery(command);
+
+                        // DB側でふられたIDを取得
+                        var newId = database1.ExecuteScalar(CommandType.Text, "SELECT @@IDENTITY");
+                        // 登録件数と、登録時にふられたIDを表示
+                        Console.WriteLine("database1: inserted: {0}, newId: {1}", count, newId);
                     }
+                    var database2 = EnterpriseLibraryContainer.Current.GetInstance<Database>();
+                    Console.WriteLine("2つ目のdatabaseオブジェクト取得 HashCode: {0}", database2.GetHashCode());
+                    for (int i = 0; i < 2; i++)
                     {
-                        var database = EnterpriseLibraryContainer.Current.GetInstance<Database>();
-                        for (int i = 0; i < 100; i++)
-                        {
-                            var p = new Person { Name = "inoue", Age = 100 };
-                            var count = database.ExecuteUpdate(
-                                "INSERT INTO PERSON(NAME, AGE) VALUES(@p0, @p1)",
-                                SequenceParameterMapper.Default,
-                                p.Name, p.Age);
-                        }
+                        // 登録対象のデータ
+                        var p = new Person { Name = "sakurai", Age = 100 };
+                        // コマンドをSQLから作成
+                        var command = database2.GetSqlStringCommand("INSERT INTO PERSON(NAME, AGE) VALUES(@p1, @p2)");
+                        // パラメータを追加
+                        database2.AddInParameter(command, "p1", DbType.String, p.Name);
+                        database2.AddInParameter(command, "p2", DbType.Int32, p.Age);
+                        // トランザクションを指定してコマンドを実行
+                        var count = database1.ExecuteNonQuery(command);
+
+                        // DB側でふられたIDを取得
+                        var newId = database2.ExecuteScalar(CommandType.Text, "SELECT @@IDENTITY");
+                        // 登録件数と、登録時にふられたIDを表示
+                        Console.WriteLine("database2: inserted: {0}, newId: {1}", count, newId);
                     }
                     tc.Complete();
+                }
+            }
+            // Transaction
+            // 例外が出るのでコメントアウト
+            //Console.WriteLine("---------------------------------------");
+            //{
+            //    using (var tc = new TransactionScope())
+            //    {
+            //        // 単純にTransactionScope内で2つのコネクションを開いて閉じる
+            //        var conn1 = new SqlCeConnection(ConfigurationManager.ConnectionStrings["SqlCe"].ConnectionString);
+            //        conn1.Open();
+            //        conn1.Close();
+            //        var conn2 = new SqlCeConnection(ConfigurationManager.ConnectionStrings["SqlCe"].ConnectionString);
+            //        conn2.Open();
+            //        conn2.Close();
+            //    }
+            //}
+            // データの登録ヘルパーメソッド
+            Console.WriteLine("---------------------------------------");
+            {
+                using (var tc = new TransactionScope())
+                {
+                    // Enterprise LibraryのコンテナからDatabaseクラスのインスタンスを取得
+                    var database = EnterpriseLibraryContainer.Current.GetInstance<Database>();
+                    // 登録対象のデータ
+                    var p = new Person { Name = "hanami", Age = 100 };
+                    // 拡張メソッドを使って登録
+                    var count = database.ExecuteUpdate(
+                        "INSERT INTO PERSON(NAME, AGE) VALUES(@p1, @p2)",
+                        p.Name, p.Age);
+                    // DB側で割り振られたIDを取得
+                    var newId = (decimal)database.ExecuteScalar(
+                        CommandType.Text, "SELECT @@IDENTITY");
+
+                    // 結果を表示
+                    Console.WriteLine("inserted: {0}, newId {1}", count, newId);
+                    tc.Complete();
+                }
+            }
+            Console.WriteLine("---------------------------------------");
+            {
+                var builder = new ConfigurationSourceBuilder();
+                builder.ConfigureData()
+                    // 使用する接続文字列を取得
+                    .ForDatabaseNamed("SqlCe")
+                    .AsDefault();
+                // Enterprise LibraryのコンテナからDatabaseクラスのインスタンスを取得
+                var database = EnterpriseLibraryContainer.Current.GetInstance<Database>();
+                // SQL文を発行してデータを格納
+                var people = database.ExecuteSqlStringAccessor<Person>(
+                    "SELECT Id, Name, Age FROM PERSON ORDER BY ID DESC");
+                // 結果を表示
+                foreach (var p in people)
+                {
+                    Console.WriteLine("Id: {0}, Name: {1}, Age: {2}", p.Id, p.Name, p.Age);
                 }
             }
         }
@@ -98,8 +194,12 @@
         public int Age { get; set; }
     }
 
+    /// <summary>
+    /// Databaseクラスへの拡張メソッド
+    /// </summary>
     static class DatabaseExtensions
     {
+        // IParameterMapperに指定したパラメータマッピングルールでSQL文を実行する
         public static int ExecuteUpdate(
             this Database self,
             string sql,
@@ -112,10 +212,19 @@
                 return self.ExecuteNonQuery(command);
             }
         }
+
+        // SequenceParameterMapperのパラメータマッピングルールでSQL文を実行する
+        public static int ExecuteUpdate(
+            this Database self,
+            string sql,
+            params object[] parameters)
+        {
+            return self.ExecuteUpdate(sql, SequenceParameterMapper.Default, parameters);
+        }
     }
 
     /// <summary>
-    /// @p0, @p1, @p2という名前の順番でパラメータをマッピングするパラメータマッパー
+    /// @p1, @p2, @p3という名前の順番でパラメータをマッピングするパラメータマッパー
     /// </summary>
     class SequenceParameterMapper : IParameterMapper
     {
@@ -131,7 +240,7 @@
                 .Select((value, index) =>
                 {
                     var p = command.CreateParameter();
-                    p.ParameterName = "p" + index;
+                    p.ParameterName = "p" + (index + 1);
                     p.Value = value;
                     return p;
                 })
