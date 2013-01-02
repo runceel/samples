dc511079285c89c9672bf03bb7546d471eae0dfe
okazuki <k.ota.0130@gmail.com>
2013-01-02 20:53:59 +0900

    コードでHello worldまで作成

---
+++ wpfedu/WPF4.5/CodeHelloWorld/App.cs
@@ -0,0 +1,30 @@
+﻿namespace CodeHelloWorld
+{
+    using System;
+    using System.Windows;
+
+    class App : Application
+    {
+        private void InitializeComponent()
+        {
+            // StartupUriは使えないのでStartupイベントを使う
+            this.Startup += App_Startup;
+        }
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            // ウィンドウを作成して表示させるコードを明示的に書く
+            var w = new MainWindow();
+            w.Show();
+        }
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            // Appクラスを作成して初期化して実行
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
---
+++ wpfedu/WPF4.5/CodeHelloWorld/MainWindow.cs
@@ -0,0 +1,46 @@
+﻿namespace CodeHelloWorld
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    class MainWindow : Window
+    {
+        private Button helloWorldButton;
+
+        private void InitializeComponent()
+        {
+            // Windowのプロパティの設定
+            this.Title = "MainWindow";
+            this.Height = 350;
+            this.Width = 525;
+
+            // Buttonの作成
+            this.helloWorldButton = new Button
+            {
+                Content = "Hello world",
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Top,
+                Margin = new Thickness(10, 10, 0, 0),
+                Width = 100
+            };
+            this.helloWorldButton.Click += helloWorldButton_Click;
+
+            // Gridの作成
+            var grid = new Grid();
+            grid.Children.Add(this.helloWorldButton);
+
+            // gridをWindowに設定
+            this.Content = grid;
+        }
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void helloWorldButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello world");
+        }
+    }
+}
