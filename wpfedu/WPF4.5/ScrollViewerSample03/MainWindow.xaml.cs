1135f3d7af81118934ce0b08f21ad82fda8b1327
okazuki <k.ota.0130@gmail.com>
2013-01-06 21:31:03 +0900

    ScrollViewerを書いた

---
+++ wpfedu/WPF4.5/ScrollViewerSample03/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace ScrollViewerSample03
+{
+    using System.Windows;
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ScrollToHalfVerticalOffsetButton_Click(object sender, RoutedEventArgs e)
+        {
+            // 垂直スクロールバーの位置を真ん中に設定
+            this.scrollViewer.ScrollToVerticalOffset(this.scrollViewer.ScrollableHeight / 2);
+        }
+    }
+}
