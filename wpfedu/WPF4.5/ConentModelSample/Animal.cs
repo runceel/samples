bca97fc60b74b59f117617b8d9d5e9d5fb39f990
okazuki <k.ota.0130@gmail.com>
2013-01-13 01:50:23 +0900

    コンテンツモデルについてちょっと説明

---
+++ wpfedu/WPF4.5/ConentModelSample/Animal.cs
@@ -0,0 +1,11 @@
+﻿namespace ConentModelSample
+{
+    using System.Windows.Media.Imaging;
+
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public BitmapImage Picture { get; set; }
+    }
+}
---
+++ wpfedu/WPF4.5/ConentModelSample/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ConentModelSample
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // オブジェクトを作成
+            var anthem = new Animal
+            {
+                Name = "アンセム",
+                Age = 9,
+                Picture = new BitmapImage(new Uri("/anthem.png", UriKind.Relative))
+            };
+
+            // ボタンに設定
+            this.buttonObject.Content = anthem;
+        }
+    }
+}
