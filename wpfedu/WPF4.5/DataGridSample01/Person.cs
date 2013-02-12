560b64ea6c431cb789abae3df73f97bd5529a2b0
okazuki <k.ota.0130@gmail.com>
2013-02-12 11:31:25 +0900

    DataGridの簡単なサンプル

---
+++ wpfedu/WPF4.5/DataGridSample01/Person.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DataGridSample01
+{
+    public class Person : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private bool SetProperty<T>(ref T holder, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (Equals(holder, value))
+            {
+                return false;
+            }
+
+            holder = value;
+            var h = this.PropertyChanged;
+            if (h != null)
+            {
+                h(this, new PropertyChangedEventArgs(propertyName));
+            }
+            return true;
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.SetProperty(ref this.age, value); }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.SetProperty(ref this.name, value); }
+        }
+
+        private string kana;
+
+        public string Kana
+        {
+            get { return this.kana; }
+            set { this.SetProperty(ref this.kana, value); }
+        }
+
+        private string address;
+
+        public string Address
+        {
+            get { return this.address; }
+            set { this.SetProperty(ref this.address, value); }
+        }
+
+        private string phone;
+
+        public string Phone
+        {
+            get { return this.phone; }
+            set { this.SetProperty(ref this.phone, value); }
+        }
+
+    }
+}
--- wpfedu/WPF4.5/ListViewSample02/SampleDataView.cs
+++ wpfedu/WPF4.5/ListViewSample02/SampleDataView.cs
@@ -6,5 +6,11 @@ namespace ListViewSample02
 {
     public class SampleDataView : DataView
     {
+        private void InitializeComponent()
+        {
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
     }
 }
