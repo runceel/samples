d002c125c4e37d9617cecc238486d671f3d2f136
Kazuki <k.ota.01330[gmail.com>
2013-04-09 12:48:48 +0900

    TreeViewのItemTemplate

---
+++ wpfedu/WPF4.5/TreeViewSample03/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TreeViewSample03
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.treeView.ItemsSource = new List<Person>
+            {
+                new Person
+                {
+                    Name = "田中　太郎",
+                    Children = new List<Person>
+                    {
+                        new Person { Name = "田中　花子" },
+                        new Person { Name = "田中　一郎" },
+                        new Person
+                        {
+                            Name = "木村　貫太郎",
+                            Children = new List<Person>
+                            {
+                                new Person { Name = "木村　はな" },
+                                new Person { Name = "木村　梅" },
+                            }
+                        }
+                    }
+                },
+                new Person
+                {
+                    Name = "田中　次郎",
+                    Children = new List<Person>
+                    {
+                        new Person { Name = "田中　三郎" }
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ wpfedu/WPF4.5/TreeViewSample03/Person.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace TreeViewSample03
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public List<Person> Children { get; set; }
+    }
+}
