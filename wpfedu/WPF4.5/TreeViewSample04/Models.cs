07bc183548d3bbe539240e0b79ee61ed0a07527c
Kazuki <k.ota.01330[gmail.com>
2013-04-19 08:36:19 +0900

    ツリーサンプル作成中

---
+++ wpfedu/WPF4.5/TreeViewSample04/Models.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeViewSample04
+{
+    public abstract class ExplorerNode
+    {
+        public string Name { get; set; }
+        public abstract int Size { get; }
+    }
+
+    public class DirectoryNode : ExplorerNode
+    {
+        public override int Size
+        {
+            get 
+            {
+                return this.ChildNodes.Sum(n => n.Size);
+            }
+        }
+        public List<ExplorerNode> ChildNodes { get; set; }
+    }
+
+    public class FileNode : ExplorerNode
+    {
+        private int size;
+
+        public FileNode(int size)
+        {
+            this.size = size;
+        }
+
+        public override int Size 
+        {
+            get { return this.size; }
+        }
+    }
+}
