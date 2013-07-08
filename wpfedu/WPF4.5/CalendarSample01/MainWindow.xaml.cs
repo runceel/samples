2a0f34c4757d30f106a76ca024196069911c0240
Kazuki <k.ota.01330[gmail.com>
2013-07-08 22:55:09 +0900

    Calenarコントロール

---
+++ wpfedu/WPF4.5/CalendarSample01/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace CalendarSample01
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(this.calendar.SelectedDate.ToString());
+        }
+    }
+}
---
+++ wpfedu/WPF4.5/CalendarSample02/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CalendarSample02
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // 今日より前は、選択不可能にする。
+            this.calendar.BlackoutDates.AddDatesInPast();
+            // 翌日から4日間も選択不可能にする
+            this.calendar.BlackoutDates.Add(
+                new CalendarDateRange(
+                    DateTime.Today.AddDays(1),
+                    DateTime.Today.AddDays(4)));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // 選択された日付を連結して表示
+            var selected = string.Join(Environment.NewLine, 
+                this.calendar.SelectedDates.Select(d => d.ToString()));
+            MessageBox.Show(selected);
+        }
+    }
+}
