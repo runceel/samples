6636247034557cca0133a6fdef9e676466cbad31
Ohta Kazuki <k.ota.0130@gmail.com>
2014-08-31 20:07:38 +0900

    アニメーションまで書いた

---
+++ wpfedu/WPF4.5/AnimationSample01/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnimationSample01
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var storyboard = new Storyboard();
+
+            var a = new DoubleAnimation();
+            // TargetName添付プロパティではなく、Target添付プロパティで
+            // 直接アニメーションのターゲットを指定しています。
+            Storyboard.SetTarget(a, rect);
+            Storyboard.SetTargetProperty(a, new PropertyPath("(Canvas.Left)"));
+            a.To = 300;
+            a.Duration = TimeSpan.FromSeconds(5);
+            storyboard.Children.Add(a);
+
+            // アニメーションを開始します
+            storyboard.Begin();
+        }
+    }
+}
