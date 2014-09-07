da4cbb102977e6223535e11c4bf13f16224d8dd5
Ohta Kazuki <k.ota.0130@gmail.com>
2014-09-07 20:09:20 +0900

    ControlTemplate

---
+++ wpfedu/WPF4.5/ResourceSample01/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ResourceSample01
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var brush = (SolidColorBrush)this.FindResource("RedBrush");
+            this.border.Background = brush;
+        }
+
+        private void DynamicButton_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Resources["AnthemBrush"] = new SolidColorBrush(Colors.Blue);
+        }
+    }
+}
