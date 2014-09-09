a40c68ac52ca5b83057574d5d286766330de923c
Ohta Kazuki <k.ota.0130@gmail.com>
2014-09-09 21:46:39 +0900

    ControlTemplate

---
+++ wpfedu/WPF4.5/CreateControlSample01/NumericUpDown.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CreateControlSample01
+{
+    /// <summary>
+    /// NumericUpDown.xaml の相互作用ロジック
+    /// </summary>
+    public partial class NumericUpDown : UserControl
+    {
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register(
+                "Value", 
+                typeof(int), 
+                typeof(NumericUpDown), 
+                new PropertyMetadata(0, ValueChanged));
+
+        private static void ValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((NumericUpDown)d).UpdateState(true);
+        }
+
+        public int Value
+        {
+            get { return (int)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public NumericUpDown()
+        {
+            InitializeComponent();
+            this.UpdateState(false);
+        }
+
+        private void UpButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Value++;
+        }
+
+        private void DownButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Value--;
+        }
+
+        private void UpdateState(bool useTransition)
+        {
+            if (this.Value >= 0)
+            {
+                VisualStateManager.GoToState(this, "Positive", useTransition);
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, "Negative", useTransition);
+            }
+        }
+    }
+}
---
+++ wpfedu/WPF4.5/CreateControlSample02/NumericUpDown.cs
@@ -0,0 +1,90 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace CreateControlSample02
+{
+    [TemplatePart(Type = typeof(RepeatButton), Name = "PART_UpButton")]
+    [TemplatePart(Type = typeof(RepeatButton), Name = "PART_DownButton")]
+    [TemplateVisualState(GroupName = "PositiveNegative", Name = "Negative")]
+    [TemplateVisualState(GroupName = "PositiveNegative", Name = "Positive")]
+    public class NumericUpDown : Control
+    {
+        static NumericUpDown()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NumericUpDown), new FrameworkPropertyMetadata(typeof(NumericUpDown)));
+        }
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register(
+                "Value", 
+                typeof(int), 
+                typeof(NumericUpDown), 
+                new PropertyMetadata(0, ValueChanged));
+
+        private static void ValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((NumericUpDown)d).UpdateState(true);
+        }
+
+
+        public int Value
+        {
+            get { return (int)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        // XAMLで定義されたボタン格納用変数
+        private RepeatButton upButton;
+        private RepeatButton downButton;
+
+        // ボタンのクリックイベント
+        private void UpClick(object sender, RoutedEventArgs e) { this.Value++; }
+        private void DownClick(object sender, RoutedEventArgs e) { this.Value--; }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            // 前のテンプレートのコントロールの後処理
+            if (this.upButton != null)
+            {
+                this.upButton.Click -= this.UpClick;
+            }
+            if (this.downButton != null)
+            {
+                this.downButton.Click -= this.DownClick;
+            }
+
+            // テンプレートからコントロールの取得
+            this.upButton = this.GetTemplateChild("PART_UpButton") as RepeatButton;
+            this.downButton = this.GetTemplateChild("PART_DownButton") as RepeatButton;
+
+            // イベントハンドラの登録
+            if (this.upButton != null)
+            {
+                this.upButton.Click += this.UpClick;
+            }
+            if (this.downButton != null)
+            {
+                this.downButton.Click += this.DownClick;
+            }
+
+            // VSMの更新
+            this.UpdateState(false);
+        }
+
+        private void UpdateState(bool useTransition)
+        {
+            if (this.Value >= 0)
+            {
+                VisualStateManager.GoToState(this, "Positive", useTransition);
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, "Negative", useTransition);
+            }
+        }
+
+    }
+}
