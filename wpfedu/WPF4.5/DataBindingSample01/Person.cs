3f152367eb59212137a9f0869e9e97b9d7e5de99
Ohta Kazuki <k.ota.0130@gmail.com>
2014-09-15 20:14:27 +0900

    DataBinding

---
+++ wpfedu/WPF4.5/DataBindingSample01/Person.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DataBindingSample01
+{
+    public class Person : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void SetProperty<T>(ref T field, T value, [CallerMemberName]string propertyName = null)
+        {
+            field = value;
+            var h = this.PropertyChanged;
+            if (h != null) { h(this, new PropertyChangedEventArgs(propertyName)); }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.SetProperty(ref this.age, value); }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.SetProperty(ref this.name, value); }
+        }
+    }
+}
---
+++ wpfedu/WPF4.5/DataBindingSample04/Person.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingSample04
+{
+    public class Person : INotifyPropertyChanged, INotifyDataErrorInfo
+    {
+        // INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            field = value;
+            var h = this.PropertyChanged;
+            if (h != null) { h(this, new PropertyChangedEventArgs(propertyName)); }
+        }
+
+        // INotifyErrorsInfo
+        private Dictionary<string, IEnumerable> errors = new Dictionary<string, IEnumerable>();
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+        private void OnErrorsChanged([CallerMemberName] string propertyName = null)
+        {
+            var h = this.ErrorsChanged;
+            if (h != null) { h(this, new DataErrorsChangedEventArgs(propertyName)); }
+        }
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            IEnumerable error = null;
+            this.errors.TryGetValue(propertyName, out error);
+            return error;
+        }
+
+        public bool HasErrors
+        {
+            get { return this.errors.Values.Any(e => e != null); }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return this.name; }
+            set 
+            { 
+                this.SetProperty(ref this.name, value); 
+                if (string.IsNullOrEmpty(value))
+                {
+                    this.errors["Name"] = new[] {"名前を入力してください" };
+                }
+                else
+                {
+                    this.errors["Name"] = null;
+                }
+                this.OnErrorsChanged();
+            }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get { return this.age; }
+            set 
+            { 
+                this.SetProperty(ref this.age, value); 
+                if (value < 0)
+                {
+                    this.errors["Age"] = new[] { "年齢は0以上を入力してください" };
+                }
+                else
+                {
+                    this.errors["Age"] = null;
+                }
+                this.OnErrorsChanged();
+            }
+        }
+
+    }
+}
