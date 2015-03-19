f7995af4a0c12ea324969b9c44f382cfc6ac2348
Kazuki <k.ota.0130@gmail.com>
2015-03-19 20:27:22 +0900

    DataGridComboBoxColumnのTips追加

---
+++ wpfedu/WPF4.5/MVVMDataGridComboboxColumnSample/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMDataGridComboboxColumnSample
+{
+    public class Person : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void SetProperty<T>(ref T field, T value, [CallerMemberName]string propertyName = null)
+        {
+            field = value;
+            var h = this.PropertyChanged;
+            if (h != null) { h(this, new PropertyChangedEventArgs(propertyName)); }
+        }
+
+        private string id;
+
+        public string Id
+        {
+            get { return this.id; }
+            set { this.SetProperty(ref this.id, value); }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.SetProperty(ref this.name, value); }
+        }
+
+        private string parentId;
+
+        public string ParentId
+        {
+            get { return this.parentId; }
+            set { this.SetProperty(ref this.parentId, value); }
+        }
+
+        public Person()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
