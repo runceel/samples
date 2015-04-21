d1902e9f3444afe11896f163a200d2e994a980c4
okazuki <k.ota.0130@gmail.com>
2015-04-21 22:13:06 +0900

    XamDataGridの最初書いた

---
+++ wpfedu/WPF4.5/XamDataGridSample01/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XamDataGridSample01
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+
+    public class MainWindowViewModel
+    {
+        public List<Person> People { get; set; }
+
+        public MainWindowViewModel()
+        {
+            this.People = Enumerable.Range(1, 1000000)
+                .Select(x => new Person
+                {
+                    Name = "okazuki " + x,
+                    Birthday = DateTime.Now,
+                    Salary = 200000 + x % 50000
+                })
+                .ToList();
+        }
+    }
+}
